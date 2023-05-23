113904bcadd5fbe136b65582996956c2fa7f7824
Abodawoud <yousefabodawoud@gmail.com>
2023-05-23 13:00:03 +0200

    valgrind

--- interactive.c
+++ interactive.c
@@ -2,7 +2,8 @@
 
 void interactive(char **argv, char *env[])
 {
-	char *line = NULL, *command, **cmd_argv = {NULL}, *environment[] = {NULL};
+	char *line = NULL, *command, *line_copy,
+	**cmd_argv = {NULL}, *environment[] = {NULL};
 	size_t len = 0;
 	ssize_t read;
 	int status = 0, count = 0, i;
@@ -17,24 +18,25 @@ void interactive(char **argv, char *env[])
 			write(1, "\n", 1);
 			break;
 		}
-		line = handle_new_line(line);
-		command = is_excutable(line);
-		if (_strcmp(line, "\n") == 0)
+		line_copy = handle_new_line(line);                        
+		command = is_excutable(line_copy);
+		if (_strcmp(line_copy, "\n") == 0)
 			continue;
-		else if (is_builtin(argv[0], line, env, &status, count) != -1)
+		else if (is_builtin(argv[0], line_copy, env, &status, count) != -1)
 			continue;
 		else if (command != NULL)
 		{
-			cmd_argv = generate_argv(line);
+			cmd_argv = generate_argv(line_copy);
 			i = excute(command, cmd_argv, environment, &status);
 			if (i == 0)
-				print_error(argv[0], count, status, line);
+				print_error(argv[0], count, status, line_copy);
 		}
 		else if (command == NULL)
 		{
-			excute_notFound(line, cmd_argv, environment, &status);
-			print_error(argv[0], count, status, line);
+			excute_notFound(line_copy, cmd_argv, environment, &status);
+			print_error(argv[0], count, status, line_copy);
 		}
+		free(command);
 	}
 	free(line);
 }
